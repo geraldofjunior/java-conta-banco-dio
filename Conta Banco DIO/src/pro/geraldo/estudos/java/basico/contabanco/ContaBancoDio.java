893ff08e8a33c3c35a6ef873b24cc472b2378060
Geraldo de Figueiredo Júnior <geraldofjunior@gmail.com>
2024-05-04 10:42:34 -0300

    Commit inicial

---
+++ Conta Banco DIO/src/pro/geraldo/estudos/java/basico/contabanco/ContaBancoDio.java
@@ -0,0 +1,7 @@
+package pro.geraldo.estudos.java.basico.contabanco;
+
+public class ContaBancoDio {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá mundo!");
+    }
+}10341
